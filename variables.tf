66e4a32c65ac3396971c47a46b5a768f56874e5f
aleksandrandreichenko <andreichenko@web.de>
2021-12-09 00:37:42 +0300

    initial variables and resources

--- variables.tf
+++ variables.tf
@@ -4,3 +4,25 @@ variable "aws_region" {
 variable "aws_vpc_name" {
   default = "vpc"
 }
+
+variable "aws_azs" {
+  description = "comma separated string of availability zones in order of precedence"
+  default     = "us-east-1a, us-east-1d, us-east-1e, us-east-1c"
+}
+
+variable "az_count" {
+  description = "number of active availability zones in VPC"
+  default     = "3"
+}
+
+variable "vpc_cidr_base" {
+  default = "10.20"
+}
+
+variable "vpc_instance_tenancy" {
+  default = "default"
+}
+
+variable "vpc_enable_dns_support" {
+  default = "true"
+}
