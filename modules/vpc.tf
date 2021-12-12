395db46d7059a8a82041bd78e8a919096fc0721b
aleksandrandreichenko <andreichenko@web.de>
2021-12-12 12:08:56 +0300

    added vpc for new Module with different parameters

--- modules/vpc.tf
+++ modules/vpc.tf
@@ -1,1 +1,13 @@
+# VPC
+resource "aws_vpc" "kube_vpc" {
+  cidr_block           = "${var.vpc_cidr}"
+  instance_tenancy     = "${var.vpc_instance_tenancy}"
+  enable_dns_support   = "${var.vpc_enable_dns_support}"
+  enable_dns_hostnames = "${var.vpc_enable_dns_hostnames}"
+  enable_classiclink   = "${var.vpc_enable_classiclink}"
+  tags                 = "${merge(local.tags, map("Name", "${var.vpc_name}"))}"
 
+  lifecycle = {
+    ignore_changes = ["tags"]
+  }
+}
