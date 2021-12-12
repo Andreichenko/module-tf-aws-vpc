44b9227439ade07e8c8c0ca79b0596fe5bb6dbf0
aleksandrandreichenko <andreichenko@web.de>
2021-12-12 12:06:31 +0300

    added variables for new Module with different parameters

--- modules/variables.tf
+++ modules/variables.tf
@@ -2,3 +2,22 @@
 variable "vpc_cidr" {
   description = "The base CIDR that all subnets and vpcs will be created from. (Format 1.2.3.4/6)"
 }
+
+variable "public_subnets_list" {
+  type        = "list"
+  description = "A list of the subnets to create for public subnets"
+}
+
+variable "private_subnets_list" {
+  type        = "list"
+  description = "A list of the subnets to create for private subnets"
+}
+
+variable "admin_subnets_list" {
+  type        = "list"
+  description = "A list of the subnets to create for admin subnets"
+}
+
+variable "vpc_name" {
+  description = "The name tag of the VPC you are creating."
+}
