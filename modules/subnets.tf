6528fdc38646f6b9901d36fbb56553e377973b21
aleksandrandreichenko <andreichenko@web.de>
2021-12-15 20:00:52 +0300

    subnets association

--- modules/subnets.tf
+++ modules/subnets.tf
@@ -7,3 +7,10 @@ resource "aws_subnet" "admin" {
   availability_zone = "${local.avail_zones_list[count.index]}"
   tags              = "${merge(local.tags, map("Name", "Admin Subnet"))}"
 }
+
+#route table association for admins
+resource "aws_route_table_association" "admin" {
+  count    = "${local._count_of_availability_zones}"
+  subnet_id   = "${element(aws_subnet.admin.*.id, count.index)}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+}
