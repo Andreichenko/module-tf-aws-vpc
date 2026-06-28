0728d25852321c0f534fce4c2ff410c5767f5ad5
andreichenko <andreichenko@web.de>
2026-06-28 20:57:51 +0300

    Fix: Add missing variables and locals definition in modules submodule to resolve terraform validate errors

--- modules/dynamics.tf
+++ modules/dynamics.tf
@@ -4,4 +4,9 @@ locals {
   public_cidr_subnets  = var.public_subnets_list
   private_cidr_subnets = var.private_subnets_list
   admin_cidr_subnets   = var.admin_subnets_list
+
+  # Added missing locals to resolve undeclared local variables errors
+  tags                         = var.global_tags
+  avail_zones_list             = var.aws_azs
+  _count_of_availability_zones = length(var.aws_azs)
 }
--- modules/variables.tf
+++ modules/variables.tf
@@ -1,5 +1,6 @@
 # Required Variables
 variable "vpc_cidr" {
+  type        = string
   description = "The base CIDR that all subnets and vpcs will be created from. (Format 1.2.3.4/6)"
 }
 
@@ -19,9 +20,42 @@ variable "admin_subnets_list" {
 }
 
 variable "vpc_name" {
+  type        = string
   description = "The name tag of the VPC you are creating."
 }
 
+# Optional Variables (Added to resolve missing parameters in submodules)
+variable "global_tags" {
+  type        = map(string)
+  default     = { "Managed By" = "Terraform" }
+  description = "AWS tags that will be added to all resources"
+}
+
+variable "aws_azs" {
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1d", "us-east-1e", "us-east-1c"]
+  description = "A list of availability zones in the region"
+}
+
+variable "vpc_instance_tenancy" {
+  type        = string
+  default     = "default"
+  description = "The allowed tenancy of instances launched into the VPC"
+}
+
+variable "vpc_enable_dns_support" {
+  type        = bool
+  default     = true
+  description = "A boolean flag to enable/disable DNS support in the VPC"
+}
+
+variable "vpc_enable_dns_hostnames" {
+  type        = bool
+  default     = true
+  description = "A boolean flag to enable/disable DNS hostnames in the VPC"
+}
+
+# Outputs
 output "aws_vpc_id" {
   value = aws_vpc.kube_vpc.id
 }
